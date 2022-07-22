d07425463c2778dbcfa675e38c7dd39967e7f222
RozhinaNadya <zhuk.09@mail.ru>
2022-07-22 16:49:03 +0200

    Added tableView, blank tableViewCell and ApiColor with singleton

---
+++ CombineIntro/ApiColor.swift
@@ -0,0 +1,17 @@
+//
+//  ApiColor.swift
+//  CombineIntro
+//
+//  Created by Надежда on 2022-07-22.
+//
+
+import Foundation
+import UIKit
+
+class ApiColor {
+    static let shered = ApiColor()
+    
+    func fetchData() {
+        
+    }
+}
--- CombineIntro/ViewController.swift
+++ CombineIntro/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CustomTableViewCell: UITableViewCell {
+    
+}
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(CustomTableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.frame = view.bounds
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CustomTableViewCell else { fatalError()
+        }
+        return cell
+    }
 
 }
 
